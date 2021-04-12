3a5ab6058b3cd71a769bce783adbed8a79e6ba76
Your Name <you@example.com>
2021-04-12 23:35:14 +0530

    styled the button component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
 export default class Navbar extends Component {
   render() {
     return (
@@ -19,15 +21,16 @@ https://www.iconfinder.com/Makoto_msk  */}
             </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-            <button>
-                <span>
+            <ButtonContainer>
+                <span className="mr-2">
                     <i className="fas fa-cart-plus">
-                        My cart
+                        my cart
                     </i>
                 </span>
-            </button>
+            </ButtonContainer>
         </Link>
       </nav>
     );
   }
 }
+
