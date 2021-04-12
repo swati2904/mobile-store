63eb77c5bf09f8c516f405802ff0b037d4cd44da
Your Name <you@example.com>
2021-04-12 22:57:49 +0530

    route setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component, Fragment } from "react";
+import { Switch, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Deatils";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import Deatils from "./components/Deatils";
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Deatils} />
+          <Route path="/Cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Deatils.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Deatils extends Component {
+    render() {
+        return (
+            <div>
+                <h3> Hello from details</h3>
+            </div>
+        )
+    }
+}
