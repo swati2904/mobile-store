efe514622d14e9e3f9b71d8c0b7ed62aa9268616
Your Name <you@example.com>
2021-04-13 21:27:33 +0530

    added card button functionality

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react'
-import Product from "./Product"
-export default class ProductList extends Component {
-    render() {
-        return (
-            <div>
-                <Product />
+import React, { Component, Fragment } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { ProductConsumer } from "../context";
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+              <ProductConsumer>
+                {(value) => {
+                  return value.products.map((product) => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
+
+export default ProductList;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ProductProvider } from "./context";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ProductProvider>
+      <Router>
+        <App />
+      </Router>
+    </ProductProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
